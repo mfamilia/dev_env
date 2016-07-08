cb85bd2e01e6f6e31a7653da5d5e4461311ad307
Manuel Familia <mfamilia@xogrp.com>
2016-07-08 12:33:35 -0400

    Cleaning up scripts

--- vim/install.sh
+++ vim/install.sh
@@ -7,11 +7,11 @@ mkdir -p $HOME/.vim/scripts
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s "$1/vim/vimrc" $HOME/.vimrc
-ln -s "$1/vim/plugins.vim" $HOME/.vim/scripts/plugins.vim
-ln -s "$1/vim/mappings.vim" $HOME/.vim/scripts/mappings.vim
+ln -s "$(pwd)/vim/vimrc" $HOME/.vimrc
+ln -s "$(pwd)/vim/plugins.vim" $HOME/.vim/scripts/plugins.vim
+ln -s "$(pwd)/vim/mappings.vim" $HOME/.vim/scripts/mappings.vim
 
-ln -s "$1/vim/ftplugin" $HOME/.vim/ftplugin
+ln -s "$(pwd)/vim/ftplugin" $HOME/.vim/ftplugin
 
 vim +PlugInstall +qall -u $HOME/.vim/scripts/plugins.vim
 vim +VimProcInstall +qall -u $HOME/.vim/scripts/plugins.vim
